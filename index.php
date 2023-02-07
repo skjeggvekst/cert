f26e5388fa168a0ec47c76d389ab7042d10f2235
Skjeggvekst <post@stiankarlsen.com>
2023-02-07 15:43:27 +0100

    Initial

---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Certification // Skjeggvekst</title>
+<link rel="stylesheet" type="text/css" href="main.css" />
+</head>
+
+<!-- 
+Website Owner: Skjeggvekst
+Website URL: http://cert.stiankarlsen.com/
+Description: Certification
+Version: 1.0
+Author: Stian Karlsen
+Author URI: http://stiankarlsen.com/
+-->
+
+<body>
+
+   <!-- Begin Wrapper -->
+   <div id="wrapper">
+		 
+		 <!-- Begin Left Column -->
+		 <div id="leftcolumn" class="borders">
+		 
+		       <!--<div class="circular"></div>-->
+		       <p class="alpha">Certification2</p>
+		       <p class="delta"><a href="https://learn.microsoft.com/nb-no/" target="_blank" rel="noopener noreferrer">Microsoft Learn</a></p>
+			   <p class="delta"><a href="https://wsr.pearsonvue.com/testtaker/registration/Dashboard/MICROSOFT" target="_blank" rel="noopener noreferrer">PearsonVue Dashboard</a></p>
+			   <p class="delta"><a href="https://learn.microsoft.com/en-us/certifications/power-platform-fundamentals/" target="_blank" rel="noopener noreferrer">PL-900</a></p>
+			   <p class="delta"><a href="https://learn.microsoft.com/en-us/certifications/security-compliance-and-identity-fundamentals/" target="_blank" rel="noopener noreferrer">SC-900</a></p>
+			   <p class="delta"><a href="https://learn.microsoft.com/en-us/certifications/microsoft-365-fundamentals/" target="_blank" rel="noopener noreferrer">MS-900</a></p>
+			   <p class="delta"><a href="https://www.microsoft.com/en-ie/training-days/azure/fundamentals" target="_blank" rel="noopener noreferrer">Microsoft Azure Virtual Training Days</a></p>
+		 
+		 </div>
+		 <!-- End Left Column -->
+		 
+		 <!-- Begin Left Middle Column -->
+		 <div id="leftmiddle">
+		       <p class="beta">Learning</p>
+		       <p class="delta"><a href="https://www.testpreptraining.com/" target="_blank" rel="noopener noreferrer">TestPrep Training</a></p>
+			   <p class="delta"><a href="https://read.amazon.co.uk/?asin=B08SJH7ZWG&language=en-GB" target="_blank" rel="noopener noreferrer">Security+ Study Guide</a></p>
+			   <p class="delta"><a href="https://www.examtopics.com/exams/comptia/sy0-601/view/" target="_blank" rel="noopener noreferrer">ExamTopics Security+</a></p>
+			   <p class="delta"><a href="https://www.examtopics.com/exams/microsoft/pl-900/view/" target="_blank" rel="noopener noreferrer">ExamTopics PL-900</a></p>
+			   <p class="delta"><a href="https://www.examtopics.com/exams/microsoft/sc-900/view/" target="_blank" rel="noopener noreferrer">ExamTopics SC-900</a></p>
+			   <p class="delta"><a href="https://www.examtopics.com/exams/microsoft/ms-900/view/" target="_blank" rel="noopener noreferrer">ExamTopics MS-900</a></p>
+			   <p class="delta"><a href="https://www.examtopics.com/exams/comptia/n10-008/view/" target="_blank" rel="noopener noreferrer">ExamTopics Network+</a></p>
+			   <p class="delta"><a href="https://www.cbtnuggets.com/it-training/comptia/security-plus" target="_blank" rel="noopener noreferrer">CBTNuggets CompTIA Security+</a></p>
+			   <p class="delta"><a href="https://www.cbtnuggets.com/it-training/microsoft-azure/sc-900" target="_blank" rel="noopener noreferrer">CBTNuggets SC-900</a></p>
+
+         </div>
+		 <!-- End Left Middle Column -->
+		 
+		 <!-- Begin Right Middle Column -->
+		 <div id="rightmiddle">
+		 	   <p class="gamma">Exams</p>
+			   <p class="delta"><a href="https://www.microsoft.com/en-us/learning/dashboard.aspx" target="_blank" rel="noopener noreferrer">Microsoft Dashboard</a></p>
+			   <p class="delta"><a href="https://wsr.pearsonvue.com/testtaker/registration/Dashboard.htm" target="_blank" rel="noopener noreferrer">PearsonVue Dashboard</a></p>
+			   <p class="delta"><a href="#" target="_blank" rel="noopener noreferrer">Exam Launchpad PL-900</a></p>
+
+		 </div>
+		 <!-- End Right Middle Column -->
+		 
+   </div>
+   <!-- End Wrapper -->
+   
+</body>
+</html>
